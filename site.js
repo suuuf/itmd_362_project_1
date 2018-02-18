51d6e7b070e9ae9a02501b9fe22c789447fa8d93
Sufyan Menk <smenk@hawk.iit.edu>
2018-02-17 23:04:20 -0600

    finished email validation

--- site.js
+++ site.js
@@ -1,5 +1,16 @@
 $("#contact-form").on("submit",
   function (e) {
     e.preventDefault();
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var email = $("#email").val();
+    if (email.length > 0) {
+      if (!emailRegex.test(email)) {
+        console.log("Invalid Email");
+      } else {
+        console.log("Valid Email; Submitting Form");
+      }
+    } else {
+      console.log("Empty Email");
+    }
   }
 );
