7f2e8f41e76cbbba352fbb13b732cf3c2c472d3b
Sufyan Menk <smenk@hawk.iit.edu>
2018-02-18 03:41:05 -0600

    Added misc styling, removed redundent styles in media query 1000, and adjusted input size in 1280 media query

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ $("html").removeClass("nojs").addClass("js");
 
 $("#contact-form").on("submit",
   function (e) {
-    var emailRegex = /^([\w-]+@([\w-]+)+[\w-]{2,4})?$/;
+    var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var email = $("#email").val();
     e.preventDefault();
     if (email.length > 0) {
