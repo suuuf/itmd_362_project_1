94999a14070e6f38716f81659183f06153a186d9
Sufyan Menk <smenk@hawk.iit.edu>
2018-02-18 00:16:24 -0600

    Passed most eslint tests

--- site.js
+++ site.js
@@ -2,9 +2,9 @@ $('html').removeClass('nojs').addClass('js');
 
 $("#contact-form").on("submit",
   function (e) {
-    e.preventDefault();
     var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     var email = $("#email").val();
+    e.preventDefault();
     if (email.length > 0) {
       if (!emailRegex.test(email)) {
         console.log("Invalid Email");
