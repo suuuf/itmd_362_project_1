25326153e6ff558a41f143b5281986de9a4099d6
Sufyan Menk <smenk@hawk.iit.edu>
2018-02-18 03:14:57 -0600

    Passed eslint

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ $('html').removeClass('nojs').addClass('js');
 
 $("#contact-form").on("submit",
   function (e) {
-    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    var emailRegex = /^([\w-]+@([\w-]+)+[\w-]{2,4})?$/;
     var email = $("#email").val();
     e.preventDefault();
     if (email.length > 0) {
@@ -10,12 +10,12 @@ $("#contact-form").on("submit",
         console.log("Invalid Email");
         if (!$("#email-section").find("li").hasClass("error")){
           $("#email-section").append("<li class='error'>Invalid email address.</li>");
-          $("#email").css("border-color","red");
+          $("#email").css("border-color", "red");
         }
       } else {
         console.log("Valid Email; Submitting Form");
         $("#email-section").find(".error").remove();
-        $("#email").css("border-color","grey");
+        $("#email").css("border-color", "grey");
         $("#contact-form").remove();
         $("#sign-up-content").append("<h2 class='success'>Signup successful!</h2>");
       }
@@ -23,7 +23,7 @@ $("#contact-form").on("submit",
       console.log("Empty Email");
       if (!$("#email-section").find("li").hasClass("error")){
         $("#email-section").append("<li class='error'>Invalid email address.</li>");
-        $("#email").css("border-color","red");
+        $("#email").css("border-color", "red");
       }
     }
   }
